3c6997b31798d523ca3bcee4416e3390f13346f3
Luis Isaac Cuautle Delgadillo <cuautle.tron@gmail.com>
2025-06-09 12:33:42 -0600

    creando los middlewares

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -1,31 +1,16 @@
 import { NextFunction, Request, Response } from "express";
-import { check, validationResult } from "express-validator";
 
 import Product from "../models/Product.model";
 
-export const createProduct = async (req: Request, res: Response, next: NextFunction) : Promise<void> => {
-  // Validacion
-  await check("name")
-    .trim()
-    .notEmpty()
-    .withMessage("El nombre del producto no debe estar vacio!")
-    .run(req);
-  
-    await check("price")
-    .isNumeric()
-    .withMessage("Valor no valido")
-    .custom( value => value > 0)
-    .withMessage("Valor no valido")
-    .notEmpty()
-    .withMessage("El nombre del producto no debe estar vacio!")
-    .run(req);
-
-  let errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
-    return
+export const createProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const product = await Product.create(req.body);
+    res.json({ data: product });
+  } catch (error) {
+    console.log(`\nOcurrio un error: ${error}\n`);
   }
-
-  const product = await Product.create(req.body);
-  res.json({ data: product });
 };
--- src/router.ts
+++ src/router.ts
@@ -1,33 +1,54 @@
 import { Router } from "express";
+import { body } from "express-validator";
 
 import { createProduct } from "./handlers/products";
+import { handleInputErrors } from "./middleware";
 
 const router = Router();
 
-router.post("/", createProduct );
+router.post(
+  "/",
+  // Validacion
+  body("name")
+    .trim()
+    .toLowerCase()
+    .notEmpty()
+    .withMessage("El nombre del producto no debe estar vacio!"),
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no valido")
+    .custom((value) => value > 0)
+    .withMessage("Valor no valido")
+    .notEmpty()
+    .withMessage("El nombre del producto no debe estar vacio!"),
+
+  // Middlewares
+  handleInputErrors,
+  createProduct
+);
 
 router.get("/", (req, res) => {
-    res.json({
-        message: "From the server post",
-    });
+  res.json({
+    message: "From the server post",
+  });
 });
 
 router.put("/", (req, res) => {
-    res.json({
-        message: "From the server put",
-    });
+  res.json({
+    message: "From the server put",
+  });
 });
 
 router.patch("/", (req, res) => {
-    res.json({
-        message: "From the server patch",
-    });
+  res.json({
+    message: "From the server patch",
+  });
 });
 
 router.delete("/", (req, res) => {
-    res.json({
-        message: "From the server delete",
-    });
+  res.json({
+    message: "From the server delete",
+  });
 });
 
 export default router;
